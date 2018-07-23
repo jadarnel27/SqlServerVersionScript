0a75f958b57084c973491600acba559ad40bfa25
Josh Darnell <jdarnell@internal.jswcoinc.com>
2018-07-22 21:46:53 -0400

    Force US english date parsing to fix date issues

--- src/GetSqlServerVersionInfo/SqlServerSupportDates.cs
+++ src/GetSqlServerVersionInfo/SqlServerSupportDates.cs
@@ -9,20 +9,24 @@ namespace GetSqlServerVersionInfo
         public SqlServerSupportDates(IReadOnlyList<string> csvData)
         {
             ProductName = csvData[0];
+
+            // All of the date parsing below are forced to use US English culture
+            // due to the CSV file from Microsoft's support site coming back 
+            // in that format regardless of the locale where the program runs
             LifeCycleStartDate = DateTime.TryParse(csvData[1], 
-                CultureInfo.CurrentCulture, DateTimeStyles.None, out var lifeCycleStartDate)
+                new CultureInfo("en-us"), DateTimeStyles.None, out var lifeCycleStartDate)
                 ? lifeCycleStartDate
                 : (DateTime?)null;
-            MainstreamSupportEndDate = DateTime.TryParse(csvData[2], 
-                CultureInfo.CurrentCulture, DateTimeStyles.None, out var mainStreamSupportEndDate)
+            MainstreamSupportEndDate = DateTime.TryParse(csvData[2],
+                new CultureInfo("en-us"), DateTimeStyles.None, out var mainStreamSupportEndDate)
                 ? mainStreamSupportEndDate
                 : (DateTime?)null;
-            ExtendedSupportEndDate = DateTime.TryParse(csvData[3], 
-                CultureInfo.CurrentCulture, DateTimeStyles.None, out var extendedSupportEndDate)
+            ExtendedSupportEndDate = DateTime.TryParse(csvData[3],
+                new CultureInfo("en-us"), DateTimeStyles.None, out var extendedSupportEndDate)
                 ? extendedSupportEndDate
                 : (DateTime?)null;
-            ServicePackSupportEndDate = DateTime.TryParse(csvData[4], 
-                CultureInfo.CurrentCulture, DateTimeStyles.None, out var servicePackSupportEndDate)
+            ServicePackSupportEndDate = DateTime.TryParse(csvData[4],
+                new CultureInfo("en-us"), DateTimeStyles.None, out var servicePackSupportEndDate)
                 ? servicePackSupportEndDate
                 : (DateTime?)null;
 
